29f5dd5cc130e80ab7ee27becd2c6424414a7ee3
Nicolas Bustamante <cnbustamante@ulp.edu.ar>
2017-10-28 21:19:13 -0300

    Scraping mejorado

--- index.js
+++ index.js
@@ -1,41 +1,65 @@
 const raw = require('tinyreq');
 const fs = require('fs');
 
-var link = "https://miproximocolectivo.sanluis.gob.ar/MiProximoColectivo/LineaIdaVuelta/?id=1&sentido=ida";
-var escribe = false;
-var simbolo = '-';
+var num = 1;
+escribirdatos(num,"ida");
+escribirdatos(num,"vuelta");
 
-raw(link, (err, body) => {
+function escribirdatos(numero,sentido){
+    var link = "https://miproximocolectivo.sanluis.gob.ar/MiProximoColectivo/LineaIdaVuelta/?id="+numero+"&sentido="+sentido;
+    raw(link, (err, body) => {
 
-    if (err || body == "") {
-        console.log(err);
-        return;
-    }
+        if (err || body == "") {
+            console.log(err);
+            return;
+        }
 
-    var test = fs.createWriteStream('test.js', {
-        flags: 'a'
+        var test = fs.createWriteStream('_'+numero+'.js', {
+            flags: 'a'
+        });
+    /*
+        body = JSON.parse(body);
+
+        body.features.map((e,i) => {
+
+            console.log(e);
+            
+        });
+    */
+        var escribe = false;
+        var i = 0;
+        if (sentido=="ida"){
+            test.write("var _"+num+"_ida = {paths: [");
+        }
+        if (sentido=="vuelta"){
+            test.write("]}var _"+num+"_vuelta = {paths: [");
+        }
+        while (body[i]!=null) {
+            //console.log(body[i]);
+            if (body[i] == "-") {
+                if (!escribe) {
+                    test.write('[');
+                }
+                escribe = true;
+            }
+            if (escribe&&body[i]!=" ") {
+                test.write(body[i]);
+                if (body[i] == ']') {
+                    test.write(',');
+                    escribe = false;
+                }
+            }
+            i++;
+        }
+        if (sentido=="vuelta"){
+            test.write("]}");
+        }
+
+        // test.write(body);
+        //console.log(body[i]);
+
     });
+}
 
-    for (var i = 0; i < 50; i++) {
-        console.log(body[i]);
-        if (body[i] == "-") {
-            if (!escribe) {
-                test.write('[');
-            }
-            escribe = true;
-        }
-        if (escribe) {
-            test.write(body[i]);
-            if (body[i] == ']') {
-                test.write(',');
-                escribe = false;
-            }
-        }
-    }
-
-    // test.write(body);
-    //console.log(body[i]);
-
-});
 
 // }
